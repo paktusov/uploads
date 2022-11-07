475830d722d38fd224c77dd54048028c7acfd339
a-paktusov@yandex.ru <83519069946>
2022-11-07 04:19:30 +0300

    performed

---
+++ app/__main__.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+
+from app.utils import allowed_file, save_image, save_resize_image
+
+UPLOAD_FOLDER = './app/static'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'bmp'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+
+@app.route('/file_list')
+def show_list():
+    if session.get('filenames'):
+        return render_template(
+            'show_list.html',
+            files=session['filenames']
+        )
+    return redirect(url_for('upload_file'))
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        files = request.files.getlist('files')
+        if files:
+            filenames = []
+            for file in files:
+                if file.filename == '':
+                    return redirect(request.url)
+                try:
+                    if allowed_file(file.filename, ALLOWED_EXTENSIONS):
+                        name, ext, filename = save_image(file, app.config['UPLOAD_FOLDER'])
+                        filenames.append(filename)
+                        resize_filename = save_resize_image(filename, ext, app.config['UPLOAD_FOLDER'])
+                        filenames.append(resize_filename)
+                    else:
+                        raise Exception
+                except:
+                    return 'Upload error'
+            session['filenames'] = filenames
+            return redirect(url_for('show_list'))
+    return render_template(
+        'upload_file.html'
+    )
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
---
+++ app/utils.py
@@ -0,0 +1,31 @@
+import datetime as dt
+import os
+from math import floor
+
+from PIL import Image
+import shortuuid
+
+
+def allowed_file(filename, exeptions):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in exeptions
+
+
+def save_image(file, path):
+    name = '.'.join(file.filename.split('.')[:-1])
+    ext = file.filename.split('.')[-1]
+    filename = f'{shortuuid.uuid()}_{dt.date.today().isoformat()}_{name}'
+    file.save(os.path.join(path, f'{filename}.{ext}'))
+    return name, ext, filename
+
+
+def save_resize_image(filename, ext, path):
+    resize_filename = f'{filename}_TN.{ext}'
+    image = Image.open(os.path.join(path, f'{filename}.{ext}'))
+    width, height = image.size
+    ratio = (height / width)
+    new_height = ratio * 500
+    # print(new_height, ratio, width, height)
+    resize_image = image.resize((500, floor(new_height)))
+    print(resize_filename)
+    resize_image.save(os.path.join(path, resize_filename))
+    return resize_filename
